803e814882e6d8aa7dc33593e5e888cc097cda12
southctrl. <134554554+SouthCtrl@users.noreply.github.com>
2024-12-31 18:17:02 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const PROGRESS = [' ', '⡀', '⣀', '⣄', '⣤', '⣦', '⣶', '⣷', '⣿'];
 
-export function progressBar(progress: number, max: number): string {
+/**
+ * Generates a text-based progress bar.
+ * @param {number} progress - The current progress.
+ * @param {number} max - The maximum value.
+ * @returns {string} The formatted progress bar.
+ */
+function ProgressBar(progress, max) {
     if (progress > max) progress = max;
     
     const percent = Math.floor((progress / max) * 100);
     const full = Math.floor(progress / max * PROGRESS.length);
     const remainder = Math.floor((progress / max * PROGRESS.length - full) * 8);
     const bar = `${PROGRESS[PROGRESS.length - 1].repeat(full)}${PROGRESS[remainder]}${' '.repeat(PROGRESS.length - full)}`;
+    
     return `[ ${bar}] ${percent}%`;
 }
+
+module.exports = ProgressBar;
